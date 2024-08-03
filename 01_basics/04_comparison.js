e1709e39e60c272c796ae5af59d1ae1a0c639fb7
naeemnz <nazifanaimie@gmail.com>
2024-08-03 18:56:06 +0200

    comaprison of different and same datatypes

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,24 @@
+/*
+console.log(2 > 1) 
+console.log(2 >= 1)
+console.log(2 < 1)
+console.log(2 == 1)
+console.log(2 != 1)
+*/
+//two different datatypes
+// console.log("2" > 1) //output: true
+// console.log("02" > 1); //output: true
+
+//comparison operators treat null as a number 0  
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+//strict check === checks (values + datatype) yahan conversion nahin hota automatically
+
+
+
